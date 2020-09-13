39b9eb925be18ac372e705cba2fc8ccfaf107909
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-13 16:11:57 +0530

    check validity,extract required outcome vector, and sort it

---
+++ rankhospital.R
@@ -0,0 +1,26 @@
+rankhospital<-function(state,outcome,num="best"){
+  
+  #read "outcome-of-care-measures" data
+  table19=read.csv("outcome-of-care-measures.csv",colClasses="character")
+  
+  #check outcome validity
+  possible_outcomes=c("heart attack","heart failure","pneumonia")
+  if(!(any(outcome==possible_outcomes)==T)) stop("invalid outcome")
+  
+  #check state code validity
+  possible_state_codes=table19[,7]
+  if(!(any(state==possible_state_codes)==T)) stop("invalid state")
+  
+  #extracting column of death rates for the given outcome in the given state
+  
+  #subsetting according to state code
+  rows_to_use<-table19[which(table19$State==state),]
+  
+  #subsetting according to outcome
+  if(outcome==possible_outcomes[1]) rates_data<-rows_to_use[,c(2,11)]
+  if(outcome==possible_outcomes[2]) rates_data<-rows_to_use[,c(2,17)]
+  if(outcome==possible_outcomes[3]) rates_data<-rows_to_use[,c(2,23)]
+  
+  #sorting rates in ascending order, using hospital names to break ties
+  sorted_rates<-rates_data[order(as.numeric(rates_data[,2]),rates_data[,1]),]
+}
