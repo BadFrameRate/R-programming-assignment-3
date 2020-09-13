e2387fc2273f3a281ac8154b770ad5d3009bffdf
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-13 17:40:29 +0530

    read data and check validity

---
+++ rankall.R
@@ -0,0 +1,13 @@
+rankall<-function(state,outcome,num="best"){
+  
+  #read "outcome-of-care-measures" data
+  table19=read.csv("outcome-of-care-measures.csv",colClasses="character")
+  
+  #check outcome validity
+  possible_outcomes=c("heart attack","heart failure","pneumonia")
+  if(!(any(outcome==possible_outcomes)==T)) stop("invalid outcome")
+  
+  #check state code validity
+  possible_state_codes=table19[,7]
+  if(!(any(state==possible_state_codes)==T)) stop("invalid state")
+}
