27c1bda945e60f779790a2d74d1b3035f4fdc544
BadFrameRate <rohatgi.aseem03@gmail.com>
2020-09-13 18:20:21 +0530

    explicit NA removal and addition of state column to use for split()

--- rankall.R
+++ rankall.R
@@ -12,9 +12,10 @@ rankall<-function(state,outcome,num="best"){
   if(!(any(state==possible_state_codes)==T)) stop("invalid state")
   
   #subsetting according to outcome
-  if(outcome=possible_outcomes[1]) all_rates<-table19[,c(2,11)]
-  if(outcome=possible_outcomes[2]) all_rates<-table19[,c(2,17)]
-  if(outcome=possible_outcomes[3]) all_rates<-table19[,c(2,23)]
+  if(outcome=possible_outcomes[1]) all_rates<-table19[,c(2,7,11)]
+  if(outcome=possible_outcomes[2]) all_rates<-table19[,c(2,7,17)]
+  if(outcome=possible_outcomes[3]) all_rates<-table19[,c(2,7,23)]
   
-  
+  #explicit NA removal
+  na_free_rates<-all_rates[!is.na(as.numeric(all_rates[,3])),]
 }
